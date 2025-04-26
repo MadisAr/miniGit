b30c5500577d58eb59aa2e05e68c0397390caf58
MadisAr <madis.arro1@gmail.com>
2025-04-26 20:15:32 +0300

    vaike muudatus

--- src/main/java/Commands/CheckoutCommand.java
+++ src/main/java/Commands/CheckoutCommand.java
@@ -1,25 +1,25 @@
-//package Commands;
-//
-//import Objects.MGitObjects.MGitObject;
-//import Objects.MiniGitRepository;
-//import Objects.ResultDTO;
-//import UtilityMethods.CreateGitSubdirectories;
-//import UtilityMethods.ReadObject;
-//
-//public class CheckoutCommand extends Command {
-//
-//    public CheckoutCommand(String[] args) {
-//        super(args);
-//    }
-//
-//    @Override
-//    public ResultDTO execute() {
-//        return null;
-//    }
-//
-//    public static void commandCheckout(String[] args) {
-//        MiniGitRepository repo = CreateGitSubdirectories.repoFind(".");
-//
-//        //MGitObject object = ReadObject.ReadObject(repo, )
-//    }
-//}
+package Commands;
+
+import Objects.MGitObjects.MGitObject;
+import Objects.MiniGitRepository;
+import Objects.ResultDTO;
+import UtilityMethods.CreateGitSubdirectories;
+import UtilityMethods.ReadObject;
+
+public class CheckoutCommand extends Command {
+
+    public CheckoutCommand(String[] args) {
+        super(args);
+    }
+
+    @Override
+    public ResultDTO execute() {
+        return null;
+    }
+
+    public static void commandCheckout(String[] args) {
+        MiniGitRepository repo = CreateGitSubdirectories.repoFind(".");
+
+        //MGitObject object = ReadObject.ReadObject(repo, )
+    }
+}

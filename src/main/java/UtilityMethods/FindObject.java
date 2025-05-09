e5c3443d7fdd664ac2a75d2f974fc585a5d38829
MadisAr <madis.arro1@gmail.com>
2025-05-09 17:53:16 +0300

    Algne natuke parem findObject command. Praegu veel veidi katki, parast teen korda.

--- src/main/java/UtilityMethods/FindObject.java
+++
@@ -1,9 +0,0 @@
-package UtilityMethods;
-
-import Objects.MiniGitRepository;
-
-public class FindObject {
-    public static String findObject(MiniGitRepository repo, String name) {
-        return name;
-    }
-}
